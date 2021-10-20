190b013a07cdbdc14fe64e3ecb850ea4ea98b043
Denis <icestrelok@gmail.com>
2021-10-20 21:58:24 +0300

    Create two TABs for Stopwatch and Timer

--- app/src/main/java/com/epluribusneo/stopwatchandtimer/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/epluribusneo/stopwatchandtimer/ui/main/SectionsPagerAdapter.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.epluribusneo.stopwatchandtimer.FragmentStopwatch;
+import com.epluribusneo.stopwatchandtimer.FragmentTimer;
 import com.epluribusneo.stopwatchandtimer.R;
 
 /**
@@ -29,7 +31,17 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 	public Fragment getItem(int position) {
 		// getItem is called to instantiate the fragment for the given page.
 		// Return a PlaceholderFragment (defined as a static inner class below).
-		return PlaceholderFragment.newInstance(position + 1);
+		//return PlaceholderFragment.newInstance(position + 1);
+		switch (position){
+			case 0:
+				FragmentStopwatch frgSW = new FragmentStopwatch();
+				return frgSW;
+			case 1:
+				FragmentTimer frgTmr = new FragmentTimer();
+				return frgTmr;
+			default:
+				return null;
+		}
 	}
 
 	@Nullable
