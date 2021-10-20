6c5f0f6603aa15a4ed3afe043b9fdc4d58559c6b
Denis <icestrelok@gmail.com>
2021-10-20 22:34:46 +0300

    Stopwatch --DONE

--- app/src/main/java/com/epluribusneo/stopwatchandtimer/FragmentStopwatch.java
+++ app/src/main/java/com/epluribusneo/stopwatchandtimer/FragmentStopwatch.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,21 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentStopwatch extends Fragment {
+
+	private Button btnStartPause;
+	private Button btnStop;
+	private TextView tvTimerValue;
+
+	private Handler customHandler = new Handler();
+
+	private long timeInMilliseconds = 0L;
+	private long timeSwapBuff = 0L;
+	private long updatedTime = 0L;
+	private long startTime = 0L;
+
+	private boolean isStarted = false;
+
+	/** *** *** *** **/
 
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -56,9 +76,75 @@ public class FragmentStopwatch extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+		//return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+		btnStartPause = (Button)rootView.findViewById(R.id.btnStartPause);
+		btnStop = (Button)rootView.findViewById(R.id.btnStop);
+		tvTimerValue = (TextView)rootView.findViewById(R.id.tvTime);
+
+
+		btnStartPause.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				timerStartStop();
+			}
+		});
+
+		btnStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startTime = 0L;
+				timeSwapBuff = 0L;
+				customHandler.removeCallbacks(updateTimeThread);
+				isStarted = false;
+				tvTimerValue.setText("00:00:000");
+				btnStartPause.setText("Start");
+			}
+		});
+
+
+		return rootView;
 	}
+
+	private void showToast(String msg){
+		Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+	}
+
+	private Runnable updateTimeThread = new Runnable() {
+		@Override
+		public void run() {
+			timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+
+			updatedTime = timeSwapBuff + timeInMilliseconds;
+
+			int seconds = (int)(updatedTime / 1000);
+			int minuts = seconds / 60;
+			seconds = seconds % 60;
+			int milliseconds = (int)(updatedTime % 1000);
+			tvTimerValue.setText("" + String.format("%02d",minuts) + ":"
+					+ String.format("%02d", seconds) + ":"
+					+ String.format("%03d", milliseconds));
+			customHandler.postDelayed(this, 0);
+		}
+	};
+
+
+	private void timerStartStop(){
+		if(isStarted){
+			timeSwapBuff += timeInMilliseconds;
+			customHandler.removeCallbacks(updateTimeThread);
+			btnStartPause.setText("Start");
+		}else{
+			startTime = SystemClock.uptimeMillis();
+			customHandler.postDelayed(updateTimeThread, 0);
+			btnStartPause.setText("Pause");
+		}
+		isStarted = !isStarted;
+	}
+
+
+
 }
