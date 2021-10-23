35bafce1b9ef164586642eea9bd3f32197e1afcb
Denis <icestrelok@gmail.com>
2021-10-23 21:11:14 +0300

    Timer --DONE

--- app/src/main/java/com/epluribusneo/stopwatchandtimer/FragmentTimer.java
+++ app/src/main/java/com/epluribusneo/stopwatchandtimer/FragmentTimer.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.os.CountDownTimer;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,22 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentTimer extends Fragment {
+
+	private TextView tvTimer;
+	private Button btnStartPause;
+	private Button btnStop;
+	private SeekBar sbMinuts;
+	private SeekBar sbSeconds;
+
+	private boolean  isStarted = false;
+	private int minutes = 0;
+	private int seconds = 0;
+	private int allSeconds = 1;
+
+	private long timeInMilliseconds = 0L;
+
+	CountDownTimer countDownTimer;
+
 
 	// TODO: Rename parameter arguments, choose names that match
 	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +80,105 @@ public class FragmentTimer extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_timer, container, false);
+		//return inflater.inflate(R.layout.fragment_timer, container, false);
+		final View rootView = inflater.inflate(R.layout.fragment_timer, container, false);
+
+		tvTimer = (TextView)rootView.findViewById(R.id.tvTimer);
+		btnStartPause = (Button)rootView.findViewById(R.id.btnStartPause);
+		btnStop = (Button)rootView.findViewById(R.id.btnStop);
+		sbMinuts = (SeekBar)rootView.findViewById(R.id.sbMinuts);
+		sbSeconds = (SeekBar)rootView.findViewById(R.id.sbSeconds);
+
+		sbSeconds.setOnSeekBarChangeListener(seekBarChangeListener);
+		sbMinuts.setOnSeekBarChangeListener(seekBarChangeListener);
+
+
+		btnStartPause.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if(isStarted == false){
+					long lll = allSeconds * 1000;
+					countDownTimer = new CountDownTimer(lll, 1000) {
+						@Override
+						public void onTick(long l) {
+							long sec  = l/1000;
+							long min = sec / 60;
+							sec = sec % 60;
+
+							sbSeconds.setProgress((int) sec);
+							sbMinuts.setProgress((int)min);
+						}
+
+						@Override
+						public void onFinish() {
+							//todo
+							sbSeconds.setProgress(0);
+							sbMinuts.setProgress(0);
+							isStarted = false;
+							theEnd();
+							showToast("FINISH");
+						}
+					}.start();
+					isStarted = true;
+					btnStartPause.setText("Pause");
+				}else{
+					theEnd();
+					isStarted = false;
+					btnStartPause.setText("Start");
+				}
+			}
+		});
+
+
+		btnStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sbMinuts.setProgress(0);
+				sbSeconds.setProgress(0);
+				countDownTimer.cancel();
+
+				isStarted = false;
+				tvTimer.setText("00:00");
+			}
+		});
+
+		return rootView;
 	}
+
+
+	SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+		@Override
+		public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+			updateTime();
+		}
+
+		@Override
+		public void onStartTrackingTouch(SeekBar seekBar) {
+
+		}
+
+		@Override
+		public void onStopTrackingTouch(SeekBar seekBar) {
+
+		}
+	};
+
+	private void updateTime(){
+		seconds = sbSeconds.getProgress();
+		minutes = sbMinuts.getProgress();
+
+		allSeconds = (int)((minutes * 60) + seconds);
+
+		tvTimer.setText("" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+	}
+
+	private void showToast(String msg){
+		Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+	}
+
+	private void theEnd(){
+		isStarted = false;
+		countDownTimer.cancel();
+	}
+
 }
